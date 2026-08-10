a923dcda1a463f421cf7a4ec26a9b4a7194e32e0
antfloc3rro <florian.antretter@c3rro.com>
2026-08-10 21:11:01 +0200

    chore(frontend): scaffold Vite + React + TypeScript + Tailwind + Vitest

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-screen">
+      <h1 className="p-4 text-xl font-semibold">Renewable Capacity Explorer</h1>
+    </div>
+  );
+}
+
+export default App;
